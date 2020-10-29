9b44ec08086f23f1a340a81656b0bab15d3752b8
eunjin0212 <eunjin9639@gmail.com>
2020-10-29 19:42:54 +0900

    Webpack-Setting-Finish

---
+++ dist/bundle.d8249f3255dfb46544fb.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\.babelrc: Error while parsing config - JSON5: invalid end of input at 1:1\\n    at syntaxError (C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\node_modules\\\\json5\\\\lib\\\\parse.js:1083:17)\\n    at invalidEOF (C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\node_modules\\\\json5\\\\lib\\\\parse.js:1032:12)\\n    at Object.start (C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\node_modules\\\\json5\\\\lib\\\\parse.js:821:19)\\n    at Object.parse (C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\node_modules\\\\json5\\\\lib\\\\parse.js:32:32)\\n    at C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\configuration.js:242:31\\n    at C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\utils.js:36:12\\n    at Generator.next (<anonymous>)\\n    at Function.<anonymous> (C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:26:3)\\n    at Generator.next (<anonymous>)\\n    at evaluateSync (C:\\\\Users\\\\eunji\\\\OneDrive\\\\Documents\\\\dev\\\\React-Study\\\\node_modules\\\\gensync\\\\index.js:251:28)\");\n\n//# sourceURL=webpack://react-study/./src/index.js?");
+/******/ })()
+;
